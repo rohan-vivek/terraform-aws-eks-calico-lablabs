9840d1053c8e29f738da3c7a007c0dab5c637df5
Rohan Vivek <150229721+rohan-vivek@users.noreply.github.com>
2024-01-30 23:49:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
-data "helm_repository" "default" {
-  depends_on = [var.mod_dependency]
-  name       = var.helm_repo_name
-  url        = var.helm_repo_url
+provider "helm" {
+  kubernetes {
+    config_path = var.kubeconfig_path
+  }
+}
+
+resource "helm_repository" "default" {
+  name = var.helm_repo_name
+  url  = var.helm_repo_url
 }
 
 resource "helm_release" "calico" {
-  depends_on = [var.mod_dependency]
+  depends_on = [helm_repository.default]
   count      = var.enabled ? 1 : 0
   name       = var.helm_release_name
-  repository = data.helm_repository.default.metadata[0].name
+  repository = helm_repository.default.metadata[0].name
   chart      = var.helm_chart_name
   namespace  = var.k8s_namespace
   version    = var.helm_chart_version
